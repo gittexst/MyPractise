566d321e9b6356400bc2ea02dc72076c56ca02a6
sudipa <s.ratha456@example.com>
2020-04-29 04:00:31 +0530

    parameterized constructer

---
+++ src/parameterized constructer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+   public  class Student
+    {
+       public int id;
+        public String name;
+
+       public Student(int i,String n){
+          id=i;
+              name=n;
+
+        }
+        public void insertData(){
+       Console.WriteLine( id + " " + name + " ");
+           
+        }
+    }
+
+    class Teacher{
+       public static void Main(string[] args)
+        {
+           Student e1= new Student(50,"raj");
+           e1.insertData();
+
+           
+        }
+    }
+    
+
+    
+    
+
