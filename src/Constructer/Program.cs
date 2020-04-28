435e282fdb966a3a9b314aee20cf8c9ed9c26b57
sudipa <s.ratha456@example.com>
2020-04-29 03:34:46 +0530

    adding default constructer

---
+++ src/Constructer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Constructer
+{
+  public  class Student
+    {
+
+    public Student()
+     {
+      Console.WriteLine("invoke default constructer");
+     }
+    }
+  class Teacher
+{
+
+          static void Main(string[] args)
+        {
+            Student s1= new Student();
+            
+            Console.WriteLine("Hello World!");
+        }
+  }
+    }
+
